bcd738878bc1627d432b0d704c871fc325bd75ce
Kiyo <kiyo@ncat.cafe>
2026-07-07 02:50:17 -0700

    pin to nixos 25.05, remove unnecessary inputs

--- flake.nix
+++ flake.nix
@@ -2,22 +2,16 @@
   description = "NixOS on Nintendo Switch";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    nixpkgs-pinned.url = "github:nixos/nixpkgs?ref=1242ee333dd7063e6d57ee1996a2e0504a28de16"; # systemd: 257.9
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
     nixos-artwork = {
       url = "github:nixos/nixos-artwork";
       flake = false;
-    };
-    deploy-rs = {
-      url = "github:serokell/deploy-rs";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs =
     {
       self,
-      deploy-rs,
       nixpkgs,
       ...
     }@inputs:
@@ -28,12 +22,5 @@
       nixosModules = import ./nixosModules inputs;
 
       formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-tree;
-      deploy.nodes.default = {
-        hostname = "nintendo-switch";
-        profiles.system = {
-          sshUser = "root";
-          path = deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations.example;
-        };
-      };
     };
 }
--- overlays/switchroot-nixos.nix
+++ overlays/switchroot-nixos.nix
@@ -9,23 +9,6 @@ let
       ln -s ${final.nvidiaPackages-l4t.alsa-config}/share/alsa/init $out/share/alsa/
     '';
   });
-  pinnedPkgs = inputs.nixpkgs-pinned.legacyPackages.${prev.stdenv.hostPlatform.system};
-
-  systemdOverride =
-    {
-      pkgFn,
-      withLogind,
-      withNspawn,
-    }:
-    let
-      pinnedPkg = pkgFn pinnedPkgs;
-      prevPkg = pkgFn prev;
-    in
-    pinnedPkg.overrideAttrs (finalAttrs: prevAttrs: {
-      passthru = prevAttrs.passthru // {
-        inherit withLogind withNspawn;
-      };
-    });
 in
 prev.lib.packagesFromDirectoryRecursive {
   callPackage = callPackage';
@@ -41,25 +24,4 @@ prev.lib.packagesFromDirectoryRecursive {
   #   '';
   # });
   pipewire-with-tegra = prev.pipewire.override { inherit alsa-lib; };
-
-  systemd = systemdOverride {
-    pkgFn = x: x.systemd;
-    withLogind = true;
-    withNspawn = true;
-  };
-  systemdMinimal = systemdOverride {
-    pkgFn = x: x.systemdMinimal;
-    withLogind = false;
-    withNspawn = false;
-  };
-  systemdLibs = systemdOverride {
-    pkgFn = x: x.systemdLibs;
-    withLogind = false;
-    withNspawn = false;
-  };
-  systemdUkify = systemdOverride {
-    pkgFn = x: x.systemdUkify;
-    withLogind = true;
-    withNspawn = true;
-  };
 }
