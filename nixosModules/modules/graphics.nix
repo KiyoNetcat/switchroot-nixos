0a220bba1b3d75c85bbe77eba8cf3ff8b6772afe
Kiyo <kiyo@ncat.cafe>
2026-07-07 21:11:52 -0700

    delete xorg server overlay because cache misses break qemu build

--- nixosModules/modules/graphics.nix
+++ nixosModules/modules/graphics.nix
@@ -61,28 +61,4 @@ with lib;
   boot.initrd.postDeviceCommands = ''
     echo 0 > /sys/class/graphics/fb0/state
   '';
-
-  nixpkgs.overlays = [
-    (final: prev: {
-      xorg = prev.xorg.overrideScope (
-        final': prev': {
-          xorgserver = prev'.xorgserver.overrideAttrs (prevAttrs: {
-            version = "1.20.14";
-            src = prev.fetchurl {
-              url = "mirror://xorg/individual/xserver/xorg-server-1.20.14.tar.gz";
-              hash = "sha256-VLGZySgP+L8Pc6VKdZZFvQ7u2nJV0cmTENW3WV86wGY=";
-            };
-            patches = prevAttrs.patches ++ [
-              # https://github.com/NixOS/nixpkgs/pull/147238
-              (prev.fetchpatch {
-                name = "stdbool.patch";
-                url = "https://gitlab.freedesktop.org/xorg/xserver/-/commit/454b3a826edb5fc6d0fea3a9cfd1a5e8fc568747.diff";
-                sha256 = "1l9qg905jvlw3r0kx4xfw5m12pbs0782v2g3267d1m6q4m6fj1zy";
-              })
-            ];
-          });
-        }
-      );
-    })
-  ];
 }
